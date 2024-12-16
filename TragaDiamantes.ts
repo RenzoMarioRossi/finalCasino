6352886039b50e28c08bb956ac703647931ce3b4
RenzoMarioRossi <167315758+RenzoMarioRossi@users.noreply.github.com>
2024-12-16 09:49:08 -0300

    sistema tragadiamantes funcional

--- TragaDiamantes.ts
+++ TragaDiamantes.ts
@@ -7,8 +7,47 @@ export class TragaDiamantes extends Tragamonedas {
         super("Tragamonedas Diamantes", 20, "Diamantes", instrucciones);
     }
 
+
+
+    protected girar(): string[] {
+        const simbolos = ["fruta", "campana", "estrella", "diamante", "limon", "7"];
+        const resultado = [
+            simbolos[Math.floor(Math.random() * simbolos.length)],
+            simbolos[Math.floor(Math.random() * simbolos.length)],
+            simbolos[Math.floor(Math.random() * simbolos.length)],
+        ];
+        return resultado;
+    }
+
     jugar(montoApuesta: number): void {
-        super.jugar(montoApuesta);
+
         console.log("¡Lluvia de diamantes! Buena suerte.");
+
+        // Simular los giros del tragamonedas
+        const resultado: string[] = this.girar();
+        console.log(`Resultados: ${resultado.join(" | ")}`);
+
+        // Comprobar si el jugador gana
+        
+        
+
+        
+        if ((resultado[0] == "diamante") && (resultado[1] == "diamante") && (resultado[2] == "diamante")) {
+            console.log("Eres la envidia de Leiva Joyas. Los diamantes coinciden, FELICIDADES!!");
+            console.log(`Ganaste $${montoApuesta * 5} !`);
+        } else if (this.esGanador(resultado)){
+            console.log("¡Felicidades! Todos los símbolos coinciden. Has ganado.");
+            console.log(`Ganaste $${montoApuesta * 2} !`);
+        } else if ((resultado[0] === resultado[1]) || (resultado[1] === resultado[2]) || (resultado[0] === resultado[2])) {
+            console.log("Dos simbolos coinciden, has salvado la apuesta!");
+            console.log(`Ganaste $${montoApuesta} !`);
+        } else {
+            console.log("Lo sentimos, no ganaste esta vez. ¡Sigue intentando!");
+        }
+
+    }
+
+    protected esGanador(resultado: string[]): boolean {
+        return resultado.every((simbolo) => simbolo === resultado[0]);
     }
 }
